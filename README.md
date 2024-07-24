617334b358a0a06f6ba99ad46a53db78559c65bf
CaptnJayce <cjestico36@gmail.com>
2024-07-24 20:58:05 +0100

    added quick sort and remaining explanations

--- README.md
+++ README.md
@@ -3,19 +3,20 @@
 
 ### download:
 1) clone repo or download zip
-2) double click index.html or run "browser_name index.html" in terminal
+2) double click index.html or run "browser_name /path/to/index.html" in terminal
 
 ### to do
 #### sorts
 - [x] bubble
 - [x] merge
-- [ ] quick
+- [x] quick
 - [x] insertion
 - [x] selection
 - [x] bogo/monkey
 - [x] miracle
 
 #### other
-- [ ] information on sorts
+- [ ] allow user to adjust speed and array size
+- [x] information on sorts
 - [ ] loading animations
 - [x] main page
--- scripts/quick.js
+++ scripts/quick.js
@@ -1,4 +1,3 @@
-
 let array = [];
 let s = 150;
 let n = 20;
@@ -6,65 +5,80 @@ let n = 20;
 init();
 
 function init(){
-  array = [];
-  for(let i = 0; i < n; i++){
-    array[i] = Math.random();
-  }
-  showBars();
+    array = [];
+    for(let i = 0; i < n; i++){
+        array[i] = Math.random();
+    }
+    showBars();
 }
 
 function play(){
-	const copy = [...array];
-	const moves = selectionSort(copy);
-	animate(moves);
+    const copy = [...array];
+    const moves = [];
+    quickSort(copy, 0, copy.length - 1, moves);
+    animate(moves);
 }
 
 function animate(moves){
-	if(moves.length == 0){
-		showBars();
-		return;
-	}
-	const swap = moves.shift();
-	const [i, j] = swap.indices;
+    if(moves.length == 0){
+        showBars();
+        return;
+    }
+    const move = moves.shift();
+    const [i, j] = move.indices;
 
-	if(swap.type == "swap"){
-		[array[i], array[j]] = [array[j], array[i]];
-	}
+    if(move.type === "swap"){
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 
-	showBars(swap);
-	setTimeout(function() {
-		animate(moves);
-	}, s);
+    showBars(move);
+    setTimeout(function() {
+        animate(moves);
+    }, s);
 }
 
-function selectionSort(array) {
-  const moves = [];
-  for (let i = 0; i < array.length - 1; i++) {
-    let lowest = i;
-    for (let j = i + 1; j < array.length; j++) {
-      moves.push({indices:[i, j], type:"comp"});
-      if (array[j] < array[lowest]) {
-        lowest = j;
-      }     
-    }
-    if (lowest !== i) {
-      [array[i], array[lowest]] = [array[lowest], array[i]];
-      moves.push({indices:[i, lowest], type:"swap"});
-    }
-  }
-  return moves;
+function partition(array, low, high, moves){
+    let pivot = array[high];
+    let i = low - 1;
+
+    for (let j = low; j <= high - 1; j++) { 
+        moves.push({indices: [j, high], type: "comp"});
+        if (array[j] < pivot) { 
+            i++;
+            moves.push({indices: [i, j], type: "swap"});
+            [array[i], array[j]] = [array[j], array[i]];
+        } 
+    } 
+    moves.push({indices: [i + 1, high], type: "swap"});
+    [array[i + 1], array[high]] = [array[high], array[i + 1]];  
+    return i + 1; 
 }
 
-function showBars(swap){
-  container.innerHTML = "";
-  for(let i = 0; i < array.length; i++){
-    const bar = document.createElement("div");
-    bar.style.height = array[i] * 100 + "%";
-    bar.classList.add("bar");
-
-    if(swap && swap.indices.includes(i)){
-      bar.style.backgroundColor = "red";
+function quickSort(array, low, high, moves) { 
+    if (low < high) {
+        let pi = partition(array, low, high, moves); 
+        moves.push({indices: [pi], type: "pivot"});
+        quickSort(array, low, pi - 1, moves); 
+        quickSort(array, pi + 1, high, moves); 
     }
-    container.appendChild(bar);
-  }
+}
+
+function showBars(move){
+    container.innerHTML = "";
+    for(let i = 0; i < array.length; i++){
+        const bar = document.createElement("div");
+        bar.style.height = array[i] * 100 + "%";
+        bar.classList.add("bar");
+
+        if(move && move.indices.includes(i)){
+            if(move.type === "swap"){
+                bar.style.backgroundColor = "red";
+            } else if(move.type === "comp"){
+                bar.style.backgroundColor = "purple";
+            } else if(move.type === "pivot"){
+                bar.style.backgroundColor = "blue";
+            }
+        }
+        container.appendChild(bar);
+    }
 }
